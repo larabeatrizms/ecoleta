83381e3f08a3a731fa36fabb58c244a54f193e08
Lara <laraamori.machado@gmail.com>
2020-06-04 01:13:30 -0300

    commit

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import PointsControllers from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express.Router();
+const pointsControllers = new PointsControllers();
+const itemsControllers = new ItemsController();
+
+routes.get("/items", itemsControllers.index);
+
+routes.post("/points", pointsControllers.create);
+routes.get("/points", pointsControllers.index);
+routes.get("/points/:id", pointsControllers.show);
+
+export default routes;
---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header title="Hello live" />
+
+      <h1>Conteúdo da app</h1>
+    </>
+  );
+}
+
+export default App;
